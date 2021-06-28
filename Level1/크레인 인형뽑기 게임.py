5bb18cc16752bd0adfab4e2f8d73be684c4dd63a
Yewon <75527311+askges20@users.noreply.github.com>
2021-06-29 00:21:52 +0900

    Update 크레인 인형뽑기 게임.py

--- Level1/크레인 인형뽑기 게임.py
+++ Level1/크레인 인형뽑기 게임.py
@@ -6,12 +6,9 @@ def solution(board, moves):
         catch = 0
         for i in range(h):
             if board[i][n-1] != 0:
-                catch = board[i][n-1] #인형 잡기
+                basket.append(board[i][n-1]) #잡은 인형 바구니에 넣기
                 board[i][n-1] = 0
                 break
-        if catch == 0: #인형을 못잡은 경우
-            continue
-        basket.append(catch) #바구니에 넣기
         if len(basket)>=2:
             if basket[-2] == basket[-1]:
                 answer += 2
