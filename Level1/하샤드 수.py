d5201a932784a606c9c2f7f2113b3355aab94cab
Yewon <askges20@kyonggi.ac.kr>
2021-09-17 23:12:45 +0900

    Update 하샤드 수.py

--- Level1/하샤드 수.py
+++ Level1/하샤드 수.py
@@ -1,4 +1,9 @@
 def solution(x):
+    return x % sum(list(map(int, list(str(x))))) == 0
+
+'''
+def solution(x):
     if x % sum(list(map(int, list(str(x))))) == 0:
         return True
     return False
+'''
