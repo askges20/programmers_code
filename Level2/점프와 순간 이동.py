0e853577680e392932afb69171fc6fe3eef79899
Yewon <askges20@kyonggi.ac.kr>
2021-10-19 17:03:35 +0900

    Update 점프와 순간 이동.py

--- Level2/점프와 순간 이동.py
+++ Level2/점프와 순간 이동.py
@@ -1,12 +1,12 @@
 def solution(n):
     cur = n
     ans = 0
-    while True:
-        if cur == 0:
-            break
+    while cur != 0:
         if cur % 2 == 0: #짝수
             cur /= 2
         else: #홀수
             ans += 1
             cur -= 1
     return ans
+
+# 또는 이진수에서 1의 개수 세기
