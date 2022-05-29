ee19d2a12e3c164d0886e27fad25f41c793e6b10
Yewon <askges20@kyonggi.ac.kr>
2022-05-29 20:01:39 +0900

    Create 우유와 요거트가 담긴 장바구니.sql

---
+++ Level4/우유와 요거트가 담긴 장바구니.sql
@@ -0,0 +1,10 @@
+SELECT A.CART_ID
+    FROM
+        (SELECT DISTINCT CART_ID
+            FROM CART_PRODUCTS
+            WHERE NAME = 'Milk') A,
+        (SELECT DISTINCT CART_ID
+            FROM CART_PRODUCTS
+            WHERE NAME = 'Yogurt') B
+    WHERE A.CART_ID = B.CART_ID
+    ORDER BY A.CART_ID
